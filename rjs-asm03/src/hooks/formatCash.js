9e87dba76d37cb32b7b83f05ea1f6617ab5b0608
viet3012 <lhv301298@gmail.com>
2024-05-21 11:14:04 +0700

    Delete rjs-asm03 directory

--- rjs-asm03/src/hooks/formatCash.js
+++
@@ -1,9 +0,0 @@
-export function formatCash(str) {
-  const string = String(str);
-  return string
-    .split("")
-    .reverse()
-    .reduce((prev, next, index) => {
-      return (index % 3 ? next : next + ".") + prev;
-    });
-}
